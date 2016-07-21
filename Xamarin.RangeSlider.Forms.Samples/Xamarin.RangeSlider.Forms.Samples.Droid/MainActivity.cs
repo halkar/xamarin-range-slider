42b515889b8b2a38e5dea5f487db2aeb2ed32c28
Artur Shamsutdinov <artur.shamsutdinov@gmail.com>
2016-07-21 23:16:33 +1000

    Added sample projects.

---
+++ Xamarin.RangeSlider.Forms.Samples/Xamarin.RangeSlider.Forms.Samples.Droid/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Xamarin.RangeSlider.Forms.Samples.Droid
+{
+    [Activity(Label = "Xamarin.RangeSlider.Forms.Samples", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
